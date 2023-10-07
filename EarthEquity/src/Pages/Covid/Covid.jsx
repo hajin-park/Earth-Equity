612a9e148ad9e91a574228b43437175e005c3248
Tofulati <almtho2003@gmail.com>
2023-10-07 12:47:55 -0700

    Added Covid and Home pages

--- EarthEquity/src/Pages/Covid/Covid.jsx
+++ EarthEquity/src/Pages/Covid/Covid.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import {LineChart, Line, XAxis, YAxis, } from "recharts";
 
 const Covid = () => {
     return (
--- EarthEquity/src/main.jsx
+++ EarthEquity/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
+import Covid from "./Pages/Covid/Covid";
+import Home from "./Pages/Home/Home";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/Covid",
+    element: <Covid />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router = {router}/>
+  </React.StrictMode>
 )
