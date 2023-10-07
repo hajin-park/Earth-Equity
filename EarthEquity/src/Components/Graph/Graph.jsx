1b9037655942105a1750ef3121105ab9f5a5b4d4
Tofulati <almtho2003@gmail.com>
2023-10-07 14:50:32 -0700

    Added axis to graph

--- EarthEquity/src/Components/Graph/Graph.jsx
+++ EarthEquity/src/Components/Graph/Graph.jsx
@@ -21,12 +21,38 @@ const Graph = ({ data, width, height }) => {
       .x((d, i) => xScale(i))
       .y(d => yScale(d));
 
-    svg.selectAll('path')
-      .data([data])
-      .join('path')
-      .attr('d', line)
+    svg.selectAll('*').remove(); // Clear previous elements
+
+    // Draw the line
+    svg.append('path')
+      .datum(data)
       .attr('fill', 'none')
-      .attr('stroke', 'blue');
+      .attr('stroke', 'blue')
+      .attr('d', line);
+
+    // Draw X axis
+    svg.append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale).ticks(data.length).tickFormat(d3.format('d')));
+
+    // Draw Y axis
+    svg.append('g')
+      .call(d3.axisLeft(yScale));
+
+    // X axis label
+    svg.append('text')
+      .attr('x', width / 2)
+      .attr('y', height + 40)
+      .style('text-anchor', 'middle')
+      .text('X Axis Label');
+
+    // Y axis label
+    svg.append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('x', -height / 2)
+      .attr('y', -40)
+      .style('text-anchor', 'middle')
+      .text('Y Axis Label');
   }, [data, width, height]);
 
   return (
