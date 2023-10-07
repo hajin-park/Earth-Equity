583da254e872a70d51c0874ac12d3858e5623256
hajin-park <hajin1819@gmail.com>
2023-10-07 11:46:30 -0700

    Initialized firebase react web app

---
+++ EarthEquity/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCby6_HtXZq4hC7mXzR05rHE8k0KkZN_v8",
+    authDomain: "earthequity.firebaseapp.com",
+    projectId: "earthequity",
+    storageBucket: "earthequity.appspot.com",
+    messagingSenderId: "586532408744",
+    appId: "1:586532408744:web:c6f45ae850b94c602e219d",
+    measurementId: "G-6RTJ8TJ1XV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
