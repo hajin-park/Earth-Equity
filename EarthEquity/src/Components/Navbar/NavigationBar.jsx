039d9ee56e8b672196ee89ecf8dcfeee6fbf7ed7
Tofulati <almtho2003@gmail.com>
2023-10-08 16:21:54 -0700

    Added gif to select days and purchase.

--- EarthEquity/src/Components/Navbar/NavigationBar.jsx
+++ EarthEquity/src/Components/Navbar/NavigationBar.jsx
@@ -90,7 +90,7 @@ export default function NavigationBar() {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                         >
-                            <Popover.Panel className="absolute -left-32 xl:-left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                            <Popover.Panel className="absolute -left-10 xl:-left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
                                 <div className="">
                                     {health.map((item) => (
                                         <div
