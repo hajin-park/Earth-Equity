ac17cddc96ff9fad3e450dec78d9de541a84e687
Hajin Park <hajin1819@gmail.com>
2023-10-08 00:31:39 -0700

    Fixed navbar bug

--- EarthEquity/src/Components/Navbar/NavigationBar.jsx
+++ EarthEquity/src/Components/Navbar/NavigationBar.jsx
@@ -1,89 +1,95 @@
-import React, { useState } from "react"
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { useState } from "react";
+import { Disclosure } from "@headlessui/react";
 import { NavLink } from "react-router-dom";
 
 const navigation = [
-    { name: "Home", to: "/"},
+    { name: "Home", to: "/" },
     { name: "Health", to: "covid" },
     { name: "Climate", to: "heat" },
-    { name: "Agriculture", to: "milk"},
+    { name: "Agriculture", to: "milk" },
 ];
 
 const NavigationBar = () => {
-    const [currentPage, setCurrentPage] = useState("Home")
+    const [currentPage, setCurrentPage] = useState("Home");
 
     const handleClick = (name) => {
-        setCurrentPage(name)
-    }
+        setCurrentPage(name);
+    };
 
     return (
         <Disclosure as="nav" className="bg-gray-800">
-        {({ open }) => (
-            <>
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                <div className="flex items-center">
-                    <a href="#" className="text-white font-medium">
-                        Earth Equity
-                    </a>
-                    <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex space-x-4">
-                        {navigation.map((item) => (
-                            <NavLink
-                                key={item.name}
-                                onClick={(e) => {
-                                    e.preventDefault()
-                                    handleClick(item.name)}
-                                }
-                                to={item.to}
-                                className={`block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white ${"bg-gray-500" ? currentPage == item.name : ""}`}
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <div className="flex h-16 items-center justify-between">
+                            <div className="flex items-center">
+                                <NavLink
+                                    to={"/"}
+                                    className="text-white font-medium"
+                                >
+                                    Earth Equity
+                                </NavLink>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <NavLink
+                                                key={item.name}
+                                                onClick={() => {
+                                                    handleClick(item.name);
+                                                }}
+                                                to={item.to}
+                                                className={`block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white ${
+                                                    currentPage == item.name
+                                                        ? "bg-gray-700"
+                                                        : ""
+                                                }`}
+                                            >
+                                                {item.name}
+                                            </NavLink>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                             >
-                                {item.name}
-                            </NavLink>
-                        ))}
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-
-            <Disclosure.Panel className="sm:hidden">
-                <div className="space-y-1 px-2 pb-3 pt-2">
-                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                    Health
-                </Disclosure.Button>
-                <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                    Health
-                </Disclosure.Button>
-                <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                    Climate
-                </Disclosure.Button>
-                <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                    Agriculture
-                </Disclosure.Button>
-                </div>
-            </Disclosure.Panel>
-            </>
-        )}
+                                Health
+                            </Disclosure.Button>
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                            >
+                                Health
+                            </Disclosure.Button>
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                            >
+                                Climate
+                            </Disclosure.Button>
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                            >
+                                Agriculture
+                            </Disclosure.Button>
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
         </Disclosure>
-    )
-}
+    );
+};
 
-export default NavigationBar
+export default NavigationBar;
--- EarthEquity/src/main.jsx
+++ EarthEquity/src/main.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  Router,
-  RouterProvider,
-} from "react-router-dom";
-import { Layout} from "./utils/layouts.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./utils/layouts.jsx";
 import Covid from "./Pages/Health/Covid";
 import Heat from "./Pages/Climate/Heat.jsx";
 import Home from "./Pages/Home/Home";
 import Milk from "./Pages/Agriculture/Milk.jsx";
-import './index.css'
+import Error from "./Pages/Error/Error.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <Error />,
-    children: [
-        {
-            path: "/",
-            element: <Home />,
-        },
-        {
-            path: "/covid",
-            element: <Covid />,
-        },
-        {
-          path: "/heat",
-          element: <Heat />,
-        },
-        {
-          path: "/milk",
-          element: <Milk />,
-        },
-    ],
-  }
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/covid",
+                element: <Covid />,
+            },
+            {
+                path: "/heat",
+                element: <Heat />,
+            },
+            {
+                path: "/milk",
+                element: <Milk />,
+            },
+        ],
+    },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router = {router}/>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
