a2a6bdb9dd4959828e07f809c1fd82809d2902eb
Tofulati <almtho2003@gmail.com>
2023-10-08 14:12:07 -0700

    Started about stocks section

--- EarthEquity/src/Pages/Home/Home.jsx
+++ EarthEquity/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./HeroSection";
 import TutorialSection from "./TutorialSection";
+import InfoSection from "./InfoSection";
 
 const Home = () => {
     return (
         <div className="bg-white px-6 py-10 lg:px-8">
             <HeroSection />
             <TutorialSection />
+            <InfoSection />
         </div>
     );
 };
