6fb4804da3bb6c0e30a359320c6d9b33a12f77ed
hajin-park <hajin1819@gmail.com>
2023-10-07 21:24:45 -0700

    Updated rio branco data processing configurations

--- Data/metereology_processing.py
+++ Data/metereology_processing.py
@@ -6,12 +6,12 @@ CSV_FILE = "LC02_Acre_Daily_Met_1970-2001.csv"
 JSON_FILE = "filtered_LC02_Acre_Daily_Met_1970-2001.json"
 filtered_data = {
     "title": "Average Temperature in Rio Branco",
-    "start": "01/04/2020",
-    "end": "09/03/2023",
+    "start": "01/01/1970",
+    "end": "05/31/2001",
     "intervals": ["Daily", "Weekly", "Monthly", "Yearly"],
-    "descriptions": ["Covid deaths"],
+    "descriptions": ["Average tempature in Rio Branco"],
     "links": [""],
-    "data": {},
+    "data": [],
 }
 
 with open(f"data/{CSV_FILE}", "r") as file:
@@ -22,3 +22,4 @@ with open(f"data/{CSV_FILE}", "r") as file:
         next(reader)
 
     for line in reader:
+        pass
